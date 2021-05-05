f0ad9e4fa8d59a73bcfc84c8906b78cc60a7f759
Khorshed Hasan Tutul <joymania_pust@outlook.com>
2021-05-05 15:23:58 +0600

    Update CartShopping.php

--- app/Model/CartShopping.php
+++ app/Model/CartShopping.php
@@ -10,5 +10,12 @@ class CartShopping extends Model
     public function product(){
         return $this->belongsTo(product::class,'product_id','id');
     }
+    public function color(){
+        return $this->belongsTo(color::class,'product_color','id');
+    }
+    public function size()
+    {
+        return $this->belongsTo(size::class, 'product_size', 'id');
+    }
 
 }
