4693295a7452a1596c7ca28171fb2435cc21501f
unknown <mdrakibul373@gmail.com>
2021-05-23 13:30:02 +0600

    admin redirection after login fixed

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -68,4 +68,18 @@ class AdminLoginController extends Controller
             : redirect('/admin/login');
     } 
 
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/admin/dashboard';
+    }
+
 }
